bbf5f97bf5fa778d7b2ae08d048737fddc529b43
Darlan Silva <darlansilv4@gmail.com>
2022-11-02 10:27:54 -0300

    consumindo banco de dados para contagem de bolões

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,20 @@
 import Fastity from 'fastify';
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient({
+    log: ['query'],
+})
 
 async function bootstrap() {
     const fastify = Fastity({
         logger: true
     });
 
-    fastify.get('/pools/count', () => {
-        return { count: 0 };
+    fastify.get('/pools/count', async () => {
+
+        const countPools = await prisma.pool.count();
+
+        return { countPools: countPools };
     });
 
     await fastify.listen({ port: 3333 });
